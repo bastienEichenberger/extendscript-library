d43f3b5acc927e6462e6670b44cc4740999294ba
bastieneichenberger <bastien.eichenberger>
2014-10-17 13:05:31 +0200

    delete files

--- tests/config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-config.app = {};
-
-
-config.app.photoshop = 'Adobe Photoshop CS6';
-config.app.indesign = 'Adobe InDesign CS6';
-config.app.illustrator = 'Adobe Illustrator CS6';
-
-module.exports = config;
