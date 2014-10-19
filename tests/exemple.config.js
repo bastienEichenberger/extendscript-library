9e199625ac1fe48ec3451637563f99cf71f60de7
bastieneichenberger <bastien.eichenberger>
2014-10-19 18:24:43 +0200

    update config and doc

--- tests/exemple.config.js
+++ tests/exemple.config.js
@@ -1,15 +1,16 @@
+
 var config = {};
-config.app = {};
+config.apps = {};
 
 /**
  * CS6
- * config.app.photoshop = 'Adobe Photoshop CS6';
- * config.app.indesign = 'Adobe InDesign CS6';
- * config.app.illustrator = 'Adobe Illustrator CS6';
+ * config.apps.photoshop = 'Adobe Photoshop CS6';
+ * config.apps.indesign = 'Adobe InDesign CS6';
+ * config.apps.illustrator = 'Adobe Illustrator CS6';
  */
 
-config.app.photoshop = 'Adobe Photoshop CC 2014';
-config.app.indesign = 'Adobe InDesign CC 2014';
-config.app.illustrator = 'Adobe Illustrator CC 2014';
+config.apps.photoshop = 'Adobe Photoshop CC 2014';
+config.apps.indesign = 'Adobe InDesign CC 2014';
+config.apps.illustrator = 'Adobe Illustrator CC 2014';
 
 module.exports = config;
