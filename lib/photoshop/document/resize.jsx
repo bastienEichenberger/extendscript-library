9c919c20eb84f6eee803f4c27f2f65544d27aff7
bastieneichenberger <bastien.eichenberger>
2015-06-30 17:32:38 +0200

    update resampling function

--- lib/photoshop/document/resize.jsx
+++ lib/photoshop/document/resize.jsx
@@ -13,19 +13,18 @@ PS.Document.Resize = (function (my) {
      * @function resampling
      * @memberOf PS.Document.Resize
      * @param {Photoshop Document} ps_doc
-     * @param {String} file_path
      * @param {Number} horizontal_scale
      * @param {Number} vertical_scale
      * @param {Number} [resolution] if the ResampleMethod is NONE the resolution is not required
      * @param {ResampleMethod} resample_method
      */
-    my.resampling = function (ps_doc, file_path, horizontal_scale, vertical_scale, resolution, resample_method) {
-        var file_path = decodeURI(file_path);
+    my.resampling = function (ps_doc, horizontal_scale, vertical_scale, resolution, resample_method) {
+
         var doc_width = ps_doc.width * (horizontal_scale / 100);
         var doc_height = ps_doc.height * (vertical_scale / 100);
-        // transform the string to enum type
+
         var resample_method = eval(resample_method);
-        // if the resample_method is NONE do not width and height
+
         if (resample_method === ResampleMethod.NONE) {
             ps_doc.resizeImage(undefined, undefined, resolution, resample_method);
         }
